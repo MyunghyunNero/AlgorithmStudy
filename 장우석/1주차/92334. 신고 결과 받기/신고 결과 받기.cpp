777089f9f56747bd11480cd28b1f2bf8a7d75229
morenow <jjang9877@gmail.com>
2023-07-13 00:13:27 +0900

    장우석 1주차 신고 결과 받기 풀이 최적화

--- 장우석/1주차/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 장우석/1주차/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -9,6 +9,7 @@ using namespace std;
 map<string, vector<string> > m;
 map<string, int> cnt;
 
+//문자열 분리
 vector<string> split(string input, char delimiter) {
     vector<string> answer;
     stringstream ss(input);
@@ -24,15 +25,8 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     for (string report1 : report) {
         //info[1] = 신고 당한 사람, info[0] = 신고한 사람
         vector<string> info = split(report1, ' ');
-        if (m.find(info[1]) != m.end()) {
-            if (find(m[info[1]].begin(), m[info[1]].end(), info[0]) == m[info[1]].end())
-                m[info[1]].push_back(info[0]);
-        }
-        else {
-            vector<string> tmp;
-            tmp.push_back(info[0]);
-            m[info[1]] = tmp;
-        }
+        if (find(m[info[1]].begin(), m[info[1]].end(), info[0]) == m[info[1]].end())
+            m[info[1]].push_back(info[0]);
     }
     for (auto it : m) {
         if (it.second.size() >= k) {
@@ -42,8 +36,7 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
         }
     }
     for (string id : id_list) {
-        if (cnt.find(id) != cnt.end()) answer.push_back(cnt[id]);
-        else answer.push_back(0);
+        answer.push_back(cnt[id]);
     }
     return answer;
 }
